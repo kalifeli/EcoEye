d8f525ecf6cd5e9edecf1115ba2ce9dfd00f6ecf
kalifeli <felizianiale34@gmail.com>
2025-03-16 16:00:04 +0100

    Interfaccia per proprietà necessarie del viewModel

---
+++ app/src/main/java/com/ecoeye/caratteristiche/bluetooth/BluetoothController.kt
@@ -0,0 +1,15 @@
+package com.ecoeye.caratteristiche.bluetooth
+
+import kotlinx.coroutines.flow.StateFlow
+
+interface BluetoothController{
+    // Dispositivi scansionati
+    val scannedDevices: StateFlow<List<DispositivoBLE>>
+    // Dispositivi accoppiati
+    val pairedDevies: StateFlow<List<DispositivoBLE>>
+
+    fun startDiscovery()
+    fun stopDiscovery()
+
+    fun release()
+}

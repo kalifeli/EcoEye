92b6a45a0a227d461329869bdbc93caa4ce2a758
kalifeli <felizianiale34@gmail.com>
2025-03-22 12:53:36 +0100

    Aggiunta possibilità per l'utente di vedere se è connesso al dispositivo, di disconnettersi se è connesso. Pulsanti per avviare e fermare la scansioni sono stati modificati

--- app/src/main/java/com/ecoeye/caratteristiche/navigazione/NavGraph.kt
+++ app/src/main/java/com/ecoeye/caratteristiche/navigazione/NavGraph.kt
@@ -1,7 +1,6 @@
 package com.ecoeye.caratteristiche.navigazione
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable

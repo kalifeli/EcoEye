1b4c2fd3d771b6ae860f0e33b2808c778aef8595
Alessandro Feliziani <106376210+kalifeli@users.noreply.github.com>
2025-04-27 18:37:31 +0200

    Update Config.kt

--- app/src/main/java/com/ecoeye/util/Config.kt
+++ app/src/main/java/com/ecoeye/util/Config.kt
@@ -3,9 +3,8 @@ package com.ecoeye.util
 import com.amazonaws.regions.Regions
 
 object Config {
-    const val AWS_IOT_ENDPOINT = "a2f2bxcm4j6lao-ats.iot.eu-central-1.amazonaws.com"
-    const val COGNITO_IDENTITY_POOL_ID = "eu-central-1:9b701385-b372-47ea-8cc2-8189d77a2b5f"
+    const val AWS_IOT_ENDPOINT = "_YOUR_AWS_IOT_ENDPOINT_"
+    const val COGNITO_IDENTITY_POOL_ID = "_YOUR_COGNITO_IDENTITY_POOL_ID_"
     val REGION = Regions.EU_CENTRAL_1
-    const val AWS_IOT_TOPIC = "esp32/trascrizione/out"
-
+    const val AWS_IOT_TOPIC = "_YOUR_AWS_IOT_SUBSCRIBE_TOPIC_"
 }
